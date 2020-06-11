f0013f706a53cd2ea8c2c6c92175dc8a467e9675
Yash Velagapudi <yashv6655@gmail.com>
2020-06-11 11:53:35 -0700

    Added Navbar

--- mern-client/src/components/Home.js
+++ mern-client/src/components/Home.js
@@ -7,5 +7,46 @@ import { UserContext } from "./Context";
 import axios from "axios";
 
 export default function Home() {
-  return <div>Hello from home page</div>;
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <Link class="navbar-brand" to="/home">
+        Simple Test Login
+      </Link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              Home <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item active">
+            <Link class="nav-link" to="/login">
+              Login
+            </Link>
+          </li>
+          <li class="nav-item active">
+            <Link class="nav-link" to="/signup">
+              Pricing
+            </Link>
+          </li>
+          <li class="nav-item active">
+            <Link class="nav-link disabled" to="/forgotpassword">
+              Disabled
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
